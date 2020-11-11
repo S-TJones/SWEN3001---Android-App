91836c4f7ebb5de020650a6eebf0f860f39d4cc0
S-TJones <jones.shemar.sj@gmail.com>
2020-11-11 12:32:02 -0500

    Added a few more line meant for the splah-screen branch

---
+++ Application/app/src/main/java/com/example/taskmanagement/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.taskmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG);
+
+        setContentView(R.layout.activity_main);
+    }
+}
