b18708f9a0787d4d0cb9c303eeb44c49f542ee4d
S-TJones <jones.shemar.sj@gmail.com>
2020-11-15 18:04:17 -0500

    Set up styles for nav-bar
    Made the main activity get rid of the original nav-bar and make it use the one created. Also created a string file for SDK 21, hopeflly it can solve the issue with the widget needing SDK 21. For some reason the default style.xml file was not generated, so I created one. It is giving errors though.

--- Application/app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ Application/app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -3,19 +3,44 @@ package com.example.taskmanagement;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity<toggle> extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    public toggle ActionBarDrawerToggle;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Use the Toolbar as Actionbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //
+        drawer = findViewById(R.id.drawer);
+        // Get menu on the left
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
     }
 }
