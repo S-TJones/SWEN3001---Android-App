044b39fa726ff7085dfb0d16cf2c014fd9091c11
S-TJones <jones.shemar.sj@gmail.com>
2020-11-14 22:37:22 -0500

    About to begin the Nav bar.

--- Application/app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ Application/app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.taskmanagement;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<toggle> extends AppCompatActivity {
+
+    public toggle ActionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
