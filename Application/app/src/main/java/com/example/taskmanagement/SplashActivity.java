d5856d228da2a1d2cbea8990c95f4e078273b64d
S-TJones <jones.shemar.sj@gmail.com>
2020-12-30 14:49:00 -0500

    Completed Splash Screen Activity
    It should load up and return to main acitivity after 5 seconds.

--- Application/app/src/main/java/com/example/taskmanagement/SplashActivity.java
+++ Application/app/src/main/java/com/example/taskmanagement/SplashActivity.java
@@ -2,17 +2,54 @@ package com.example.taskmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    // Variables
+    Animation topAnimation, bottomAnimation;
+    ImageView appImage;
+    TextView appLogo, appSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
+        // Animations
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        // Hooks
+        appImage = findViewById(R.id.imageView);
+        appLogo = findViewById(R.id.name);
+        appSlogan = findViewById(R.id.slogan);
+
+        // Setting Animations
+        appImage.setAnimation(topAnimation);
+        appLogo.setAnimation(bottomAnimation);
+        appSlogan.setAnimation(bottomAnimation);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
