b367c9ac405987237c72fd8a1fe67ac4c8884139
S-TJones <jones.shemar.sj@gmail.com>
2020-12-29 22:21:48 -0500

    Added a floating button

--- Application/app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ Application/app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.taskmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton addButton = findViewById(R.id.add_activity);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
 }
