fa9dbdbfd4ec3cd33226d9b38c81fe55758a4055
S-TJones <jones.shemar.sj@gmail.com>
2020-12-31 18:34:57 -0500

    Added the AddActivity Java file

---
+++ Application/app/src/main/java/com/example/taskmanagement/AddActivity.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add2);
+    }
+}
