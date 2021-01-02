d2b9463cc0ae9fdf37014643d379edcfdeb45bfd
S-TJones <jones.shemar.sj@gmail.com>
2021-01-02 10:59:15 -0500

    Added Sub-Task Database
    Created the Database files for Managing Sub-Tasks and the neccessary methods needed. Right now they don't work with both (Main and Sub) ID being the primary key. I will have to sort that out later.

--- Application/app/src/main/java/com/example/taskmanagement/Database/MainTaskDatabase.java
+++ Application/app/src/main/java/com/example/taskmanagement/Database/MainTaskDatabase.java
@@ -8,19 +8,22 @@ import androidx.annotation.Nullable;
 
 public class MainTaskDatabase extends SQLiteOpenHelper {
 
-    public static final String table_name = "MAIN_ACTIVITY";
+    public static final int VERSION = 1;
+    public static final String DB_NAME = "taskManagementDatabase";
+    public static final String table_name = "Main_Tasks";
     public static final String table_column_ID = "ID";
     public static final String table_column_TITLE = "Title";
     public static final String table_column_DATE = "Date";
-    public static final String table_column_TIME = "Time";
+    public static final String table_column_HOUR = "Hour";
+    public static final String table_column_MINUTE = "Minute";
 
     public MainTaskDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, "main-activity.db", null, 1);
+        super(context, DB_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE IF NOT EXISTS " + table_name + " (" + table_column_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + table_column_TITLE + " TEXT, " + table_column_DATE + " VARCHAR, " + table_column_TIME + " VARCHAR)";
+        String createTable = "CREATE TABLE " + table_name + " (" + table_column_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + table_column_TITLE + " TEXT, " + table_column_DATE + " VARCHAR, " + table_column_HOUR + " VARCHAR, " + table_column_MINUTE + " VARCHAR)";
         db.execSQL(createTable);
 
     }
---
+++ Application/app/src/main/java/com/example/taskmanagement/Database/SubTaskDatabase.java
@@ -0,0 +1,97 @@
+package com.example.taskmanagement.Database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.taskmanagement.Classes.SubTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubTaskDatabase extends SQLiteOpenHelper {
+
+    public static final int VERSION = 1;
+    public static final String DB_NAME = "taskManagementDatabase";
+    public static final String table_name = "Sub_Tasks";
+    public static final String table_column_MainID = "MainID"; // Remember to add this as primary key
+    public static final String table_column_ID = "ID";
+    public static final String table_column_NAME = "Name";
+    public static final String table_column_STATUS = "Status";
+
+    private SQLiteDatabase db;
+
+    public SubTaskDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + table_name + " (" + table_column_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + table_column_NAME + " TEXT, " + table_column_STATUS + " INTEGER)";
+        db.execSQL(createTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        // Drop the old table to create a new one
+        db.execSQL("DROP TABLE IF EXISTS " + table_name);
+
+        // Create the new table
+        onCreate(db);
+    }
+
+    public void openDatabase(){
+        db = this.getWritableDatabase();
+    }
+
+    public void insertSubTask(SubTask task){
+        ContentValues cv = new ContentValues();
+        cv.put(table_column_NAME, task.getSub_task());
+        cv.put(table_column_STATUS, 0);
+        db.insert(table_name, null, cv);
+    }
+
+    public List<SubTask> getAllTasks(){
+        List<SubTask> taskList = new ArrayList<>();
+
+        db.beginTransaction();
+        try (Cursor cursor = db.query(table_name, null, null, null, null, null, null, null)) {
+            if (cursor.moveToFirst()) {
+                do {
+                    SubTask task = new SubTask(0, cursor.getInt(cursor.getColumnIndex(table_column_ID)));
+                    task.setSub_task(cursor.getString(cursor.getColumnIndex(table_column_NAME)));
+                    task.setStatus(cursor.getInt(cursor.getColumnIndex(table_column_STATUS)));
+                    taskList.add(task);
+                } while (cursor.moveToNext());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
+
+        return taskList;
+    }
+
+    public void updateStatus(int id, int status){
+        ContentValues cv =  new ContentValues();
+        cv.put(table_column_STATUS, status);
+        db.update(table_name, cv, table_column_ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+    public void updateSubTask(int id, String newName){
+        ContentValues cv =  new ContentValues();
+        cv.put(table_column_NAME, newName);
+        db.update(table_name, cv, table_column_ID + "=?", new String[] {String.valueOf(id)});
+    }
+
+    public void deleteSubTask(int id){
+        db.delete(table_column_NAME, table_column_ID + "=?", new String[] {String.valueOf(id)});
+    }
+}
